c55bd8614315182a459ffdc69568c03fa27bf736
andevidal <andevidal@iutclinfa2304l.iut.local>
2020-11-27 15:28:37 +0100

    ajouts des fichiers

--- programme/src/console/mainConsole.java
+++ programme/src/console/mainConsole.java
@@ -3,18 +3,28 @@ package console;
 import game.Board;
 import game.Game;
 
+import java.util.Random;
+
 public class mainConsole {
     public static void main(String[] args) {
-        Game g = new Game();
 
-        g.placeBoats(0,0,true);
-        g.placeBoats(0,1,true);
-        g.placeBoats(0,2,true);
-        g.placeBoats(0,3,true);
-        g.placeBoats(0,4,true);
-        g.placeBoats(0,5,true);
+        Random r = new Random();
+        int x=0,y=0;
 
-        System.out.println(g.getPlayer());
+        System.out.println(r.nextInt((4-1) + 1) );
+        System.out.println(r.nextInt((4-1) + 1) );
+        System.out.println(r.nextInt((4-1) + 1) );
+        System.out.println(r.nextInt((4-1) + 1) );
+        System.out.println(r.nextInt((4-1) + 1) );
+        System.out.println(r.nextInt((4-1) + 1) );
+        System.out.println(r.nextInt((4-1) + 1) );
+        System.out.println(r.nextInt((4-1) + 1) );
+        System.out.println(r.nextInt((4-1) + 1) );
+        System.out.println(r.nextInt((4-1) + 1) );
+        System.out.println(r.nextInt((4-1) + 1) );
+
+
+
     }
 
 
